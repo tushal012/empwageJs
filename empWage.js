2b5ddeda9e2c69deb39f4743a57b02e4f7f2eb43
tushal kumar <tushalkashyap11@gmail.com>
2021-07-01 11:08:09 -0700

    Refactored : code to write function to get work hour

--- empWage.js
+++ empWage.js
@@ -1,23 +1,24 @@
+//uc3
+
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
-const PART_TIME_HOUR = 4;
-const FULL_TIME_HOUR = 8;
-const WAGE_PER_HOUR = 20;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20; 
 
-let empHr = 0;
-
-let empCheck = Math.floor(Math.random() * 10 ) % 3;
-
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHr = PART_TIME_HOUR;
-        break;
-    case IS_FULL_TIME:
-        empHr = FULL_TIME_HOUR;
-        break;
-    default :
-    empHr = 0;
+function getWorkingHours(empCheck) {
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
 
-let empWage = empHr * WAGE_PER_HOUR;
-console.log("Employee wage is :" + empWage)
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage: " + empWage)
